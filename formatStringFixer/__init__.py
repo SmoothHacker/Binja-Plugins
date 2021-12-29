96dd1f0410b2a111704ff5b43ef71be504dc5e67
SmoothHacker <laglerscott@gmail.com>
2021-12-28 18:56:56 -0600

    fix: modify listRef for get_code_refs

--- formatStringFixer/__init__.py
+++ formatStringFixer/__init__.py
@@ -5,11 +5,11 @@ from binaryninja import *
 def fixPrintf(bv):
   printfSymbol = bv.get_symbol_by_raw_name("printf")
 
-  printfRefs = bv.get_code_refs(printfSymbol.address)
+  printfRefList= bv.get_code_refs(printfSymbol.address)
   t = bv.parse_type_string("const char [0x3]")
 
-  for ref in printfRefs:
-    LLIL_instr = ref.function.get_low_level_il_at(ref.address)
+  for printfRef in printfRefList:
+    LLIL_instr = printfRef.function.get_low_level_il_at(printfRef.address)
     fmtStringAddr = LLIL_instr.high_level_il.operands[1][0].operands[0]
     bv.define_data_var(fmtStringAddr, t[0])
 
